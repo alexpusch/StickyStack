0e8375c6cb9f2926c63964ac6a25554464ed07a7
Alex Puschinksy <alexpoo@gmail.com>
2016-04-15 21:37:36 +0300

    Create proejct

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path')
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+  entry: "./src/sticky_stack.js",
+  output: {
+      path: "dist",
+      filename: "stickyStack.js",
+      libraryTarget: "umd",
+      publicPath: "/dist/",
+      library: "StickyStack",
+      umdNamedDefine: true,
+
+  },
+  devtool: "#inline-source-map",
+  module : {
+    loaders: [ {
+        test   : /.js$/,
+        loader : 'babel-loader',
+        exclude: /node_modules/,
+        query  : {
+          presets: ['es2015'],
+          plugins: [
+            'add-module-exports'
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+  ]
+}
